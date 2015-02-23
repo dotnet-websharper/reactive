8043fdf58b121329d0f8e9909c59afdf83202dc6
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-02-23 11:32:47 +0100

    Initial commit: split off main WebSharper repository

---
+++ build.fsx
@@ -0,0 +1,25 @@
+#load "tools/includes.fsx"
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("IntelliFactory.Reactive", "3.0-alpha")
+    |> fun bt -> bt.WithFramework(bt.Framework.Net40)
+
+let main =
+    bt.WebSharper.Library("IntelliFactory.Reactive")
+        .SourcesFromProject()
+
+bt.Solution [
+    main
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Title = Some "IntelliFactory.Reactive"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://github.com/intellifactory/reactive"
+                Description = "Reactive Library for WebSharper"
+                RequiresLicenseAcceptance = true })
+        .Add(main)
+]
+|> bt.Dispatch
