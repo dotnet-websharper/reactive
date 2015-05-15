c56a9f93d8d26f732d829601d24ceb90767c5cc5
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-05-15 10:19:11 +0200

    Get version from WebSharper

--- build.fsx
+++ build.fsx
@@ -2,8 +2,9 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("IntelliFactory.Reactive", "3.0")
-    |> fun bt -> bt.WithFramework(bt.Framework.Net40)
+    BuildTool().PackageId("IntelliFactory.Reactive")
+        .VersionFrom("WebSharper")
+        .WithFramework(fun fw -> fw.Net40)
 
 let main =
     bt.WebSharper.Library("IntelliFactory.Reactive")
