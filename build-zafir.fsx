827b5badc94ea19fe4c570dc1288a3918ea71ff5
István Gansperger <gansperger@gmail.com>
2015-12-11 15:27:17 +0100

    Add zafir build

---
+++ build-zafir.fsx
@@ -0,0 +1,27 @@
+#load "tools/includes.fsx"
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("Zafir.Reactive")
+        .VersionFrom("Zafir")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
+        .WithFramework(fun fw -> fw.Net40)
+
+let main =
+    bt.Zafir.Library("IntelliFactory.Reactive")
+        .SourcesFromProject()
+
+bt.Solution [
+    main
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Title = Some "Zafir.Reactive"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://github.com/intellifactory/reactive"
+                Description = "Reactive Library for Zafir"
+                RequiresLicenseAcceptance = true })
+        .Add(main)
+]
+|> bt.Dispatch
