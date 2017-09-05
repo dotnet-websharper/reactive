ce751fce8d3f5468d6dbd8bb60406040c1433912
Jand42 <andras.janko@gmail.com>
2017-09-05 22:50:09 +0200

    Rename Zafir packages to WebSharper

--- build-zafir.fsx
+++ build-zafir.fsx
@@ -2,13 +2,13 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("Zafir.Reactive")
-        .VersionFrom("Zafir")
+    BuildTool().PackageId("WebSharper.Reactive")
+        .VersionFrom("WebSharper")
         .WithFSharpVersion(FSharpVersion.FSharp30)
         .WithFramework(fun fw -> fw.Net40)
 
 let main =
-    bt.Zafir.Library("IntelliFactory.Reactive")
+    bt.WebSharper4.Library("IntelliFactory.Reactive")
         .SourcesFromProject()
         .WithSourceMap()
 
@@ -18,10 +18,10 @@ bt.Solution [
     bt.NuGet.CreatePackage()
         .Configure(fun c ->
             { c with
-                Title = Some "Zafir.Reactive"
+                Title = Some "WebSharper.Reactive"
                 LicenseUrl = Some "http://websharper.com/licensing"
                 ProjectUrl = Some "https://github.com/intellifactory/reactive"
-                Description = "Reactive Library for Zafir"
+                Description = "Reactive Library for WebSharper"
                 RequiresLicenseAcceptance = true })
         .Add(main)
 ]
